0f9b0f4d32002b7f7e4e78045f9fe2f28254b1c0
Weldon Carlos <weldonprogrammer2022@gmail.com>
2026-06-30 15:24:28 -0300

    Integração de navegação

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -5,20 +5,27 @@ import {
   Typography,
   Box,
 } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 interface GameCardProps {
+  id: number;
   name: string;
   image: string;
   released: string;
 }
 
 export function GameCard({
+  id,
   name,
   image,
   released,
 }: GameCardProps) {
+
+  const navigate = useNavigate();
+
   return (
     <Card
+      onClick={() => navigate(`/game/${id}`)}
       sx={{
         height: "100%",
         backgroundColor: "background.paper",
