adabf4f24d6c88e0baa36affce6d79a44e002fcf
Weldon Carlos <weldonprogrammer2022@gmail.com>
2026-06-30 15:27:54 -0300

    Refaturação e integração de search

--- src/services/gameService.ts
+++ src/services/gameService.ts
@@ -4,17 +4,35 @@ import type { Game } from "../types/Game";
 const API_KEY = import.meta.env.VITE_RAWG_API_KEY;
 const BASE_URL = "https://api.rawg.io/api";
 
-export async function getGames(page: number): Promise<Game[]> {
+export async function getGames(
+  page: number,
+  search = ""
+): Promise<Game[]> {
+
   const response = await axios.get<{ results: Game[] }>(
     `${BASE_URL}/games`,
     {
       params: {
         key: API_KEY,
         page,
-        page_size: 8,
+        search,
+       
       },
     }
   );
 
   return response.data.results;
+}
+
+export async function getGameById(id: string): Promise<Game> {
+  const response = await axios.get<Game>(
+    `${BASE_URL}/games/${id}`,
+    {
+      params: {
+        key: API_KEY,
+      },
+    }
+  );
+
+  return response.data;
 }
